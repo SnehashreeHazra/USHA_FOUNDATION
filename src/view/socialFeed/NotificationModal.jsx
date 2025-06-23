f61e6172cb5d7e49b66ed60de8b45f4008ab2282
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-23 17:48:25 +0530

    'up'

--- src/view/socialFeed/NotificationModal.jsx
+++ src/view/socialFeed/NotificationModal.jsx
@@ -6,12 +6,12 @@ const NotificationModal = () => {
     <div className="notification-modal">
       <div className="notification-modal-container">
         <div className="notification-modal-head">
-          <p>Notification</p>
+          <p className="notification">Notifications</p>
           <img src={threeDot} alt="" />
         </div>
         <div className="notify-modal-head2">
-          <div className="unread-btn">All</div>
-          <div>Unread</div>
+          <div className="unread-btn active">All</div>
+          <div className="unread-btn">Unread</div>
         </div>
       </div>
     </div>
