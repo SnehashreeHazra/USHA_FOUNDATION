f62ed55505d12ba45ace0349efc8a719d99388f5
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-06-23 19:12:59 +0530

    'up'

--- src/view/socialFeed/NotificationModal.jsx
+++ src/view/socialFeed/NotificationModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import threeDot from "../../assets/Group 2 1.png";
+import priyaImg from "../../assets/Group 25 (1).png"
 
 const NotificationModal = () => {
   return (
@@ -12,6 +13,15 @@ const NotificationModal = () => {
         <div className="notify-modal-head2">
           <div className="unread-btn active">All</div>
           <div className="unread-btn">Unread</div>
+        </div>
+        <div className="notification-modal-body">
+          <div>
+            <img src={priyaImg} alt="" />
+            <div>
+              <p><span>Priya Vishwanathan and 4 other people</span> recently  liked your article.</p>
+              <p>10h</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
