0276fa9fd974c0e996070963f8be80d6c3d9e858
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-01 16:08:42 +0530

    'up'

--- src/view/LandingPage/LandingBanner.jsx
+++ src/view/LandingPage/LandingBanner.jsx
@@ -7,7 +7,7 @@ const LandingBanner = () => {
       <div className="uni-container">
         <div className="landing-banner-text">
           <p className="landing-banner-heading">THE POWER OF ONE</p>
-          <p className="landing-banner-subheading">THE ONE FOR EVERYONE</p>
+          <p className="landing-banner-subheading">THE ONE PLACE FOR EVERYONE</p>
           <div className="banner-about-us-btn">
             <span>About Us!</span>
             <img src={yellowArrow} alt="" />
--- src/view/LandingPage/LandingPage.jsx
+++ src/view/LandingPage/LandingPage.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import './LandingPage.css'
-import LandingBanner from './LandingBanner'
-import placeEveryone from "../../assets/Group 180.png"
+import React from "react";
+import "./LandingPage.css";
+import LandingBanner from "./LandingBanner";
+import placeEveryone from "../../assets/Group 180.png";
+import yellowArrow from "../../assets/Group 181.png";
 
 const LandingPage = () => {
   return (
     <div>
       <LandingBanner />
       <div className="place-for-everyone">
-        <div className="place-for-everyone-left">
-          <p>The ONE PLACE FOR EVERYONE</p>
-          <p>USHA is the dawn, the usher of new light and hope. Ushaism doctrine will be a consciousness raiser, based on science, reason, and spirituality. It's a platform for free thinkers, atheist and theists to learn, engage in dialogues, develop an unbiased understanding of reality and create a common future for humanity.</p>
-        </div>
-        <div className="place-for-everyone-right">
-          <img src={placeEveryone} alt="" />
+        <div className="uni-container place-for-every-container">
+          <div className="place-for-everyone-left">
+            <p className="place-everyone-heading">The ONE PLACE FOR EVERYONE</p>
+            <p className="place-everyone-text">
+              USHA is the dawn, the usher of new light and hope. Ushaism
+              doctrine will be a consciousness raiser, based on science, reason,
+              and spirituality. It's a platform for free thinkers, atheist and
+              theists to learn, engage in dialogues, develop an unbiased
+              understanding of reality and create a common future for humanity.
+            </p>
+            <div className="banner-about-us-btn">
+              <span>Sign up now!</span>
+              <img src={yellowArrow} alt="" />
+            </div>
+          </div>
+          <div className="place-for-everyone-right">
+            <img src={placeEveryone} alt="" />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
