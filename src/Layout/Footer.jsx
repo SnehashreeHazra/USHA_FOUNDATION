4ebba955f95cb22941b604365437e94fd08d393e
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-07 17:37:24 +0530

    'up'

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import brownArrow from "../assets/Group 190.png";
 import youtubeImg from "../assets/Group 189.png";
-import ushaLogo from "../assets/USA New Logo 1 2.png"
+import ushaLogo from "../assets/USA New Logo 1 2.png";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="uni-container footer-container">
         <div className="footer-left">
-          <p className="usha-foundation-heading">USHA <span>FOUNDATION</span></p>
+          <p className="usha-foundation-heading">
+            USHA <span>FOUNDATION</span>
+          </p>
           <div className="footer-left-separator"></div>
-          <p className="usha-spiritual-foundation">UNIVERSAL SPIRITUAL HUMAN AWAKENING</p>
+          <p className="usha-spiritual-foundation">
+            UNIVERSAL SPIRITUAL HUMAN AWAKENING
+          </p>
         </div>
         <div className="footer-middle">
           <img src={ushaLogo} alt="" />
@@ -23,7 +27,7 @@ const Footer = () => {
           <p className="footer-subscribe">Subscribe</p>
           <img className="youtube-img" src={youtubeImg} alt="" />
           <p className="join-family">Join our Worldfamily.</p>
-          <p style={{fontSize:"13px"}}>
+          <p style={{ fontSize: "13px" }}>
             Subscribe to our YouTube and social media handles for free for all
             updates.
           </p>
