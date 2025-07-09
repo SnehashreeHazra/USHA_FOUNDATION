939fb27a8a2c52388ffd35c8ebd4b04abb9ab2a1
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-09 15:53:03 +0530

    'up'

--- src/view/LandingPage/LandingPage.jsx
+++ src/view/LandingPage/LandingPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./LandingPage.css";
 import LandingBanner from "./LandingBanner";
-import placeEveryone from "../../assets/Group 180.png";
+import placeEveryone from "../../assets/Group 198.png";
 import blackArrow from "../../assets/Group 206.png";
-import ushaStream from "../../assets/Group 183.png";
-import blueArrow from "../../assets/Group 188.png"
-import ushaSparks from "../../assets/Group 182.png"
-import ushaInsights from "../../assets/Group 187.png"
+import ushaStream from "../../assets/Group 199.png";
+import blueArrow from "../../assets/Group 190.png";
+import ushaSparks from "../../assets/Group 200.png";
+import ushaInsights from "../../assets/Group 201.png";
 import LatestEvent from "./LatestEvent";
 import Footer from "../../Layout/Footer";
+import yellowArrow from "../../assets/Group 194.png";
 
 const LandingPage = () => {
   return (
@@ -31,7 +32,7 @@ const LandingPage = () => {
             </div>
           </div>
           <div className="place-for-everyone-right">
-            {/* <img src={placeEveryone} alt="" /> */}
+            <img src={placeEveryone} alt="" />
           </div>
         </div>
       </div>
@@ -55,8 +56,10 @@ const LandingPage = () => {
 
       <div className="uni-container usha-stream">
         <div className="usha-sparks-left">
-        <p className="usha-stream-heading" style={{textAlign:'left'}}>USHA Sparks</p>
-          <p className="usha-stream-text" style={{textAlign:'left'}}>
+          <p className="usha-stream-heading" style={{ textAlign: "left" }}>
+            USHA Sparks
+          </p>
+          <p className="usha-stream-text" style={{ textAlign: "left" }}>
             Watch timeless stories and civilisational insights come alive
             through original documentaries, series, and visual essays. Streaming
             Vedic wisdom for the modern seeker.
@@ -65,7 +68,6 @@ const LandingPage = () => {
             <span>Ignite Your Mind</span>
             <img src={blueArrow} alt="" />
           </div>
-          
         </div>
         <div className="usha-sparks-right">
           <img src={ushaSparks} alt="" />
@@ -79,9 +81,9 @@ const LandingPage = () => {
         <div className="usha-stream-right">
           <p className="usha-stream-heading">USHA Insights</p>
           <p className="usha-stream-text">
-            Read transformative articles, decoded scriptures, and cross-cultural thought-pieces from modern Dhārmic thinkers.
-Ancient wisdom meets modern inquiry.
-
+            Read transformative articles, decoded scriptures, and cross-cultural
+            thought-pieces from modern Dhārmic thinkers. Ancient wisdom meets
+            modern inquiry.
           </p>
           <div className="explore-original-btn">
             <span>Explore Articles</span>
@@ -91,6 +93,24 @@ Ancient wisdom meets modern inquiry.
       </div>
 
       <LatestEvent />
+      <div className="uni-container">
+        <p className="quarry-form-heading">For any quarry or collaboration</p>
+        <form className="quarry-form">
+          <div className="about-form-group">
+            <input type="text" placeholder="Full Name" />
+            <input type="text" placeholder="Email address" />
+          </div>
+          <div className="about-form-group">
+            <input type="text" placeholder="Country" />
+            <input type="text" placeholder="Language" />
+          </div>
+          <button className="quarry-form-btn">
+            <span>Contact Now!</span>
+            <img src={yellowArrow} alt="" />
+          </button>
+        </form>
+      </div>
+
       <Footer />
     </div>
   );
