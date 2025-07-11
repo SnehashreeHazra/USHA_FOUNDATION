bb3cf0de1175c6fccc7e3df81748f1993e025666
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-11 13:11:11 +0530

    'up'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import MainLayout from "./MainLayout/MainLayout";
