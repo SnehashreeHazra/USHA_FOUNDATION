d784cf3e6dc6baf7ba6eb825bb6e7c31d447cd58
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-11 17:39:17 +0530

    'up'

--- src/view/LandingPage/LatestEvent.jsx
+++ src/view/LandingPage/LatestEvent.jsx
@@ -1,31 +1,55 @@
-import React from 'react'
-import BigImage from "../../assets/image 36.png"
-import blueArrow from "../../assets/Group 190.png"
-import cardImg from "../../assets/Snana Cover 16.png"
+import React from "react";
+import BigImage from "../../assets/image 36.png";
+import blueArrow from "../../assets/Group 190.png";
+import cardImg from "../../assets/Snana Cover 16.png";
 
 const LatestEvent = () => {
   return (
-    <div className='latest-event'>
-    <div className="uni-container">
-
-      <img className='latest-event-big-img' src={BigImage} alt="" />
-      <p className='latest-event-heading'>LATEST EVENTS</p>
-      <div className="latest-events-cards">
-        <div className="latest-event-card">
-          <img className='latest-event-card-img' src={cardImg} alt="" />
-          <div className='latest-event-card-text'>
-            <p className='upcoming-text'>Upcoming Events</p>
-            <p className='event-heading'>Kumbh Kahini</p>
-            <p className='event-process'>Online</p>
-            <div className='event-card-btn'><span>23rd June - 5pm</span><img src={blueArrow} alt="" /></div>
+    <div className="latest-event">
+      <div className="uni-container">
+        <img className="latest-event-big-img" src={BigImage} alt="" />
+        <p className="latest-event-heading">LATEST EVENTS</p>
+        <div className="latest-events-cards">
+          <div className="latest-event-card active">
+            <img className="latest-event-card-img" src={cardImg} alt="" />
+            <div className="latest-event-card-text">
+              <p className="upcoming-text active">Upcoming Events</p>
+              <p className="event-heading">Kumbh Kahini</p>
+              <p className="event-process">Online</p>
+              <div className="event-card-btn">
+                <span>23rd June - 5pm</span>
+                <img src={blueArrow} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="latest-event-card">
+            <img className="latest-event-card-img" src={cardImg} alt="" />
+            <div className="latest-event-card-text">
+              <p className="upcoming-text">Upcoming Events</p>
+              <p className="event-heading">Kumbh Kahini</p>
+              <p className="event-process">Online</p>
+              <div className="event-card-btn">
+                <span>23rd June - 5pm</span>
+                <img src={blueArrow} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="latest-event-card">
+            <img className="latest-event-card-img" src={cardImg} alt="" />
+            <div className="latest-event-card-text">
+              <p className="upcoming-text">Upcoming Events</p>
+              <p className="event-heading">Kumbh Kahini</p>
+              <p className="event-process">Online</p>
+              <div className="event-card-btn">
+                <span>23rd June - 5pm</span>
+                <img src={blueArrow} alt="" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="latest-event-card"></div>
-        <div className="latest-event-card"></div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default LatestEvent
+export default LatestEvent;
