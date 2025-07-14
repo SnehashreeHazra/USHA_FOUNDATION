15e43980cc81c2a8e1f16a711b9a8381012b6a18
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-14 14:55:12 +0530

    'up'

--- src/view/VedicKnowledge/VedicKnowledge.jsx
+++ src/view/VedicKnowledge/VedicKnowledge.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './VedicKnowledge.css'
+import React from "react";
+import "./VedicKnowledge.css";
 
 const VedicKnowledge = () => {
   return (
     <div>
-      
+      <div className="vedic-knowledge-banner">
+        <div className="uni-container" >
+          <div className="social-feed-navbar">
+            <p className="active">USHA Stream</p>
+            <p>USHA Sparks</p>
+            <p>USHA Insights</p>
+            <p>USHA Dialouge</p>
+            <p>USHA Social</p>
+            <p>USHA Academy</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VedicKnowledge
+export default VedicKnowledge;
