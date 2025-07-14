26f15ba1594b3a4d810feaf7891e5f60b4dd1719
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-14 15:03:57 +0530

    'up'

--- src/view/VedicKnowledge/VedicKnowledge.jsx
+++ src/view/VedicKnowledge/VedicKnowledge.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./VedicKnowledge.css";
+import saveIcon from "../../assets/SAve (White) 1.png";
 
 const VedicKnowledge = () => {
   return (
     <div>
       <div className="vedic-knowledge-banner">
-        <div className="uni-container" >
+        <div className="uni-container">
           <div className="social-feed-navbar">
             <p className="active">USHA Stream</p>
             <p>USHA Sparks</p>
@@ -13,6 +14,22 @@ const VedicKnowledge = () => {
             <p>USHA Dialouge</p>
             <p>USHA Social</p>
             <p>USHA Academy</p>
+          </div>
+
+          <div className="knowledge-container">
+            <div className="knowledge-left">
+              <p>NEW EPISODE</p>
+              <p>
+                Decoding Snan | The Sacred Bathing of Transformation | Hinduism
+                Explained
+              </p>
+            </div>
+            <div className="knowledge-right">
+              <button>Watch Now</button>
+              <div>
+                <button>Trailer</button> <img src={saveIcon} alt="" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
