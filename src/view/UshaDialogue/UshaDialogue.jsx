52f068731b072a91c309e29255fb3cbb27519b1b
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-15 17:01:54 +0530

    'up'

--- src/view/UshaDialogue/UshaDialogue.jsx
+++ src/view/UshaDialogue/UshaDialogue.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./UshaDialogue.css";
-import saveIcon from "../../assets/SAve (White) 1.png";
+import saveIcon from "../../assets/SAve (Brown) 1.png";
 import latestEpisodeImg1 from "../../assets/Snana Cover 16.png";
 import latestEpisodeImg2 from "../../assets/Thumbnail 1.png";
 import latestEpisodeImg3 from "../../assets/Openning Video thumbnail 1.png";
@@ -8,6 +8,12 @@ import sliderBtn from "../../assets/Arrow 79.png";
 import latestDialogueImg1 from "../../assets/image 170.png";
 import latestDialogueImg2 from "../../assets/image 171.png";
 import latestDialogueImg3 from "../../assets/image 172.png";
+import vishnuImg from "../../assets/Group 210.png";
+import vishnuImg2 from "../../assets/Group 209.png";
+import vishnuImg3 from "../../assets/Group 211.png";
+import upcomingImg1 from "../../assets/image 133.png";
+import upcomingImg2 from "../../assets/image 138.png";
+import upcomingImg3 from "../../assets/image 139.png";
 import Footer from "../../Layout/Footer";
 import { useNavigate } from "react-router-dom";
 
@@ -34,14 +40,14 @@ const UshaDialogue = () => {
           <div className="knowledge-left">
             <p className="new-episode">NEW EPISODE</p>
             <p className="decoding-snan">
-              Decoding Snan | The Sacred Bathing of Transformation | Hinduism
-              Explained
+              The Construct of Maya, Mind, and the Inevitability of God | Pinaki
+              Gangopadhyay
             </p>
           </div>
           <div className="knowledge-right">
             <button className="knowledge-watch-btn">Watch Now</button>
             <div className="knowledge-right-bottom-div">
-              <button className="knowledge-trailer-btn">Trailer</button>{" "}
+              <button className="knowledge-trailer-btn00">Trailer</button>{" "}
               <img src={saveIcon} alt="" />
             </div>
           </div>
@@ -55,7 +61,7 @@ const UshaDialogue = () => {
           also the mind, soul, and karma... Read More
         </p>
 
-        <div>
+        <div className="dialogue-divs">
           <p className="latest-epi-heading">LATEST DIALOGUES</p>
           <div className="latest-episodes-div">
             <img className="episode" src={latestDialogueImg1} alt="" />
@@ -65,17 +71,17 @@ const UshaDialogue = () => {
           </div>
         </div>
 
-        <div>
+        <div className="dialogue-divs">
           <p className="latest-epi-heading">VISHNU AVATARS-EPISODES</p>
           <div className="latest-episodes-div">
-            <img className="episode" src={latestEpisodeImg1} alt="" />
-            <img className="episode" src={latestEpisodeImg2} alt="" />
-            <img className="episode" src={latestEpisodeImg3} alt="" />
+            <img className="episode" src={vishnuImg} alt="" />
+            <img className="episode" src={vishnuImg2} alt="" />
+            <img className="episode" src={vishnuImg3} alt="" />
             <img className="slider-arrow-btn" src={sliderBtn} alt="" />
           </div>
         </div>
 
-        <div>
+        <div className="dialogue-divs">
           <p className="latest-epi-heading">SCIENCE IN RITUALS</p>
           <div className="latest-episodes-div">
             <img className="episode" src={latestEpisodeImg1} alt="" />
@@ -85,12 +91,12 @@ const UshaDialogue = () => {
           </div>
         </div>
 
-        <div>
+        <div className="dialogue-divs">
           <p className="latest-epi-heading">UPCOMING SEASONS</p>
           <div className="latest-episodes-div">
-            <img className="episode" src={latestEpisodeImg1} alt="" />
-            <img className="episode" src={latestEpisodeImg2} alt="" />
-            <img className="episode" src={latestEpisodeImg3} alt="" />
+            <img className="episode" src={upcomingImg1} alt="" />
+            <img className="episode" src={upcomingImg2} alt="" />
+            <img className="episode" src={upcomingImg3} alt="" />
             <img className="slider-arrow-btn" src={sliderBtn} alt="" />
           </div>
         </div>
