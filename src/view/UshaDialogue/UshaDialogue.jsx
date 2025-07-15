01eb436e3add83fab42c1de97df6387e921b096c
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-15 12:24:03 +0530

    'up'

--- src/view/UshaDialogue/UshaDialogue.jsx
+++ src/view/UshaDialogue/UshaDialogue.jsx
@@ -6,7 +6,21 @@ const UshaDialogue = () => {
   return (
     <>
       <div className="dialogue-banner">
-        <div className="uni-container"></div>
+        <div className="uni-container">
+          <div className="social-feed-navbar">
+              <p
+                onClick={() => navigate("/vedic-knowledge")}
+                className="active"
+              >
+                USHA Stream
+              </p>
+              <p onClick={() => navigate("/vedic-reels")}>USHA Sparks</p>
+              <p onClick={() => navigate("/blog")}>USHA Insights</p>
+              <p onClick={() => navigate("/usha-dialogue")}>USHA Dialogue</p>
+              <p>USHA Social</p>
+              <p>USHA Academy</p>
+            </div>
+        </div>
       </div>
       <Footer />
     </>
