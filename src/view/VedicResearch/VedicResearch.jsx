edde1043e779243be0aba3a4b0f094237bacbc8c
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-07-16 16:59:29 +0530

    'up'

--- src/view/VedicResearch/VedicResearch.jsx
+++ src/view/VedicResearch/VedicResearch.jsx
@@ -3,6 +3,7 @@ import "./VedicResearch.css";
 import Footer from "../../Layout/Footer";
 import yellowArrow from "../../assets/Group 194.png";
 import videoImg from "../../assets/image 70.png";
+import aiImg from "../../assets/Group 217.png";
 
 const VedicResearch = () => {
   useEffect(() => {
@@ -34,7 +35,44 @@ const VedicResearch = () => {
               <img src={yellowArrow} alt="" />
             </div>
             <img className="research-video" src={videoImg} alt="" />
+            <p>ASK USHA</p>
+            <div className="ask-usha-btn">
+              <img src={aiImg} alt="" /> <span>Ask USHA</span>
+            </div>
+            <p>
+              Ask USHA is the platform’s GenAI-powered engine that leverages
+              cutting-edge Large Language Models (LLMs) trained on curated Vedic
+              texts, commentaries, and cross-cultural knowledge frameworks. It
+              is designed to intelligently find, decode, and reinterpret ancient
+              Sanskrit ślokas, philosophical concepts, rituals, and cosmological
+              insights—making them accessible, contextual, and applicable for
+              today’s seeker. By integrating semantic search, multilingual
+              understanding, and dialogue-based exploration, Ask USHA acts as a
+              personalised dhārmic guide, enabling users to ask profound
+              questions and receive nuanced, scripturally grounded
+              responses—bridging 5,000 years of civilisational wisdom with the
+              intelligence of tomorrow. It is a living interface between ancient
+              consciousness and modern curiosity.
+            </p>
+            <div className="mission-why-usha-btn">
+              <span className="active">Research</span> <span>Projects</span>
+            </div>
           </div>
+          <p className="quarry-form-heading">For any quarry or collaboration</p>
+          <form className="quarry-form">
+            <div className="about-form-group">
+              <input type="text" placeholder="Full Name" />
+              <input type="text" placeholder="Email address" />
+            </div>
+            <div className="about-form-group">
+              <input type="text" placeholder="Country" />
+              <input type="text" placeholder="Language" />
+            </div>
+            <button className="quarry-form-btn">
+              <span>Contact Now!</span>
+              <img src={yellowArrow} alt="" />
+            </button>
+          </form>
         </div>
       </div>
       <Footer />
